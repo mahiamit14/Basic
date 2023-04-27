79aa7df9b3d4248b3173f9674de59934ff07459c
Amit Kumar Thakur <amitt2117gmail.com>
2023-04-27 08:09:50 +0530

    added html file

---
+++ basic.js
@@ -0,0 +1,27 @@
+// var a = "5";
+// var b = "10";    
+// /*Do not change the code above*/
+
+// //Write your code here
+// var c=a;
+// a=b;
+// b=c;
+// /***********Do not change the code below 👇*******/
+// console.log("a is " + a);
+// console.log("b is " + b);
+
+if( 0) {
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+// for(var i=0; i<5; ++i) { 
+//     console.log("Hello") ; 
+//  }
+
+//  var i , j ;    
+//    for(i=0, j=10; i<5; ++i) { 
+//       console.log("Hello"); 
+//    }
+// typeof( typeof( typeof( 100 ) ) )
